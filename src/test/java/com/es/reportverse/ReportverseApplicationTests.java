a9b4074e334ae97c8dc2bb685ad8637bfbeb0d5a
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-06-14 22:05:48 -0300

    Initial commit

---
+++ src/test/java/com/es/reportverse/ReportverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.reportverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
