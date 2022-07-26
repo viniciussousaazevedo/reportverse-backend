288d65959599f1a2986c484ab9c37b6421ae0533
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-07-26 16:20:02 -0300

    Revert "Update ReportverseApplication.java"
    This reverts commit 74199f4612c7a471c5eacefd9333414fce30c0be.

--- src/main/java/com/es/reportverse/ReportverseApplication.java
+++ src/main/java/com/es/reportverse/ReportverseApplication.java
@@ -2,10 +2,7 @@ package com.es.reportverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -15,15 +12,5 @@ public class ReportverseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportverseApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
-			}
-		};
 	}
 }
