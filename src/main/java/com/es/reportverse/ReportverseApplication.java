74199f4612c7a471c5eacefd9333414fce30c0be
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-07-26 16:14:09 -0300

    Update ReportverseApplication.java

--- src/main/java/com/es/reportverse/ReportverseApplication.java
+++ src/main/java/com/es/reportverse/ReportverseApplication.java
@@ -2,7 +2,10 @@ package com.es.reportverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,5 +15,15 @@ public class ReportverseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportverseApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+			}
+		};
 	}
 }
